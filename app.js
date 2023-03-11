55ffef82b32f1fb47af32795d74554ffe2d15a89
ekremcaglayan <ekremcaglayan1@hotmail.com>
2023-03-11 21:56:11 +0300

    MongoDB Atlas

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ekremcaglayan1:Test123@cluster0.mg2ny9r.mongodb.net/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
     title: String,
